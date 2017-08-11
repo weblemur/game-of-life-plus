92c7d08d283ff09d38f4bdbd75ffcf8af15122a3
Tim He <theshuo@gmail.com>
2017-08-11 09:12:20 -0500

    need to fix drawing possibly

--- README.md
+++
@@ -1,8 +0,0 @@
-# wasm-intro/add
-
-A trivial example of:
-
-- Defining a WASM function in C
-- Loading a WASM module and calling a WASM function from JavaScript
-
-The page loads the WASM module and calls it to add two integers together.
--- gol.js
+++ gol.js
@@ -14,44 +14,112 @@ function fetchAndInstantiate(url, importObject) {
     );
 }
 */
+let outergame;
 
 function main() {
-    let width = 500;
-    let height = 500;
+
+    let width = 20;
+    let height = 20;
     //let ctx = draw();
     let game = new GameOfLife(width, height);
+    outergame = game;
     game.randomFill();
-    draw(game);
+    //game.step();
+    game.step();
+    //draw(game);
 }
 
-function draw(game) {
-    var canvas = document.getElementById('board');
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'green';
-    for (let x = 0; x < game._width; x++) {
-        for (let y = 0; y < game._height; y++) {
-            if (game.getCellState(x, y))
-                ctx.fillRect(x, y, 1, 1);
-        }
-    }
-}
 
 class GameOfLife {
 
-    constructor(width, height) {
-        this._width = width;
-        this._height = height;
-        this._board = new Uint8Array(width * height);
-        this._nextBoard = new Uint8Array(width * height);
+    draw() {
+
+        let ctx = this.ctx;
+        let imageData = this.imageData;
+        for (let i = 0; i < this._board.length; i++) {
+            if (this._board[i]) imageData.data[i * 4 + 3] = 255;
+        }
+
+        // disable smoothing on scaling
+        ctx.imageSmoothingEnabled = false;
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+
+        // create, scale, and draw image
+        createImageBitmap(imageData)
+            .then(img => {
+                ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+                ctx.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight);
+            });
     }
 
-    step() {
 
+
+    constructor(width, height) {
+
+        this._width = width;
+        this._height = height;
+
+        this.canvasWidth = 500;
+        this.canvasHeight = 500;
+
+        this._board = new Uint8Array(width * height);
+        this._nextBoard = new Uint8Array(width * height);
+        this.canvas = document.getElementById('board');
+        this.ctx = this.canvas.getContext('2d');
+        this.imageData = this.ctx.createImageData(this._width, this._height);
     }
 
     getCellState(x, y) {
-        return this._board[x + y * this._width] > 0;
+        return this._board[this.getIndex(x, y)];
     }
+
+    swap(x) {
+        return x
+    };
+
+    /* Main loop */
+    step() {
+        this.draw();
+        this.updateCells();
+        requestAnimationFrame(this.step.bind(this));
+    }
+
+    updateCells() {
+        for (let x = 0; x < this._width; x++) {
+            for (let y = 0; y < this._height; y++) {
+                this.setNextState(x, y);
+            }
+        }
+        let temp = this._board;
+        this._board = this._nextBoard;
+        this._nextBoard = temp;
+        // this._board = this.swap(this._board, this._board = this._nextBoard);
+    }
+
+    setNextState(x, y) {
+
+        let aliveNeighbors = this.getAliveNeighbors(x, y);
+        // console.log('aliveneighbors:', aliveNeighbors, 'x, y: ', x, y);
+
+        if (this.getCellState(x, y)) {
+            if (aliveNeighbors < 2) {
+                this._nextBoard[this.getIndex(x, y)] = 0;
+            }
+            if (aliveNeighbors === 2 || aliveNeighbors === 3) {
+                this._nextBoard[this.getIndex(x, y)] = 1;
+            }
+            if (aliveNeighbors > 3) {
+                this._nextBoard[this.getIndex(x, y)] = 0;
+            }
+        } else {
+            if (aliveNeighbors === 3) {
+                this._nextBoard[this.getIndex(x, y)] = 1;
+            }
+        }
+    }
+
 
     getIndex(x, y) {
         return x + y * this._width;
@@ -77,18 +145,10 @@ class GameOfLife {
             var neighborY = y + offset[1];
 
             // Periodic boundary conditions
-            if (neighborX < 0) {
-                neighborX = this._width - 1;
-            }
-            if (neighborX >= this._width) {
-                neighborX = 0;
-            }
-            if (neighborY < 0) {
-                neighborY = this._height - 1;
-            }
-            if (neighborY >= this._height) {
-                neighborY = 0;
-            }
+            if (neighborX < 0) neighborX = this._width - 1;
+            if (neighborX >= this._width) neighborX = 0;
+            if (neighborY < 0) neighborY = this._height - 1;
+            if (neighborY >= this._height) neighborY = 0;
 
             if (neighborX >= 0 && neighborX < this._width) {
                 if (neighborY >= 0 && neighborY < this._height) {
@@ -106,7 +166,7 @@ class GameOfLife {
 
     randomFill() {
         for (let i = 0; i < this._board.length; i++) {
-            if (Math.random() < 1)
+            if (Math.random() < Math.random() * 0.2)
                 this._board[i] = 1;
             else
                 this[i] = 0;
